8e1105929327f14735efa7da768e26b829bb47a9
giovanni.ssilva7 <giovanni.ssilva7@SBC0505232W10-1>
2022-05-17 15:48:06 -0300

    Adicionar arquivos de projeto.

---
+++ nomeArquivo/Form.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace nomeArquivo
+{
+    partial class Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEn = new System.Windows.Forms.Label();
+            this.lblDe = new System.Windows.Forms.Label();
+            this.lblPa = new System.Windows.Forms.Label();
+            this.btnEn = new System.Windows.Forms.Button();
+            this.btnTrocar = new System.Windows.Forms.Button();
+            this.tbxEn = new System.Windows.Forms.TextBox();
+            this.tbxDe = new System.Windows.Forms.TextBox();
+            this.tbxPa = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lblEn
+            // 
+            this.lblEn.AutoSize = true;
+            this.lblEn.Location = new System.Drawing.Point(64, 68);
+            this.lblEn.Name = "lblEn";
+            this.lblEn.Size = new System.Drawing.Size(56, 15);
+            this.lblEn.TabIndex = 0;
+            this.lblEn.Text = "Endereço";
+            // 
+            // lblDe
+            // 
+            this.lblDe.AutoSize = true;
+            this.lblDe.Location = new System.Drawing.Point(64, 142);
+            this.lblDe.Name = "lblDe";
+            this.lblDe.Size = new System.Drawing.Size(21, 15);
+            this.lblDe.TabIndex = 1;
+            this.lblDe.Text = "De";
+            // 
+            // lblPa
+            // 
+            this.lblPa.AutoSize = true;
+            this.lblPa.Location = new System.Drawing.Point(64, 216);
+            this.lblPa.Name = "lblPa";
+            this.lblPa.Size = new System.Drawing.Size(30, 15);
+            this.lblPa.TabIndex = 2;
+            this.lblPa.Text = "Para";
+            // 
+            // btnEn
+            // 
+            this.btnEn.Location = new System.Drawing.Point(488, 68);
+            this.btnEn.Name = "btnEn";
+            this.btnEn.Size = new System.Drawing.Size(71, 26);
+            this.btnEn.TabIndex = 4;
+            this.btnEn.Text = "...";
+            this.btnEn.UseVisualStyleBackColor = true;
+            this.btnEn.Click += new System.EventHandler(this.btnEn_Click);
+            // 
+            // btnTrocar
+            // 
+            this.btnTrocar.Location = new System.Drawing.Point(477, 136);
+            this.btnTrocar.Name = "btnTrocar";
+            this.btnTrocar.Size = new System.Drawing.Size(82, 95);
+            this.btnTrocar.TabIndex = 5;
+            this.btnTrocar.Text = "Trocar";
+            this.btnTrocar.UseVisualStyleBackColor = true;
+            this.btnTrocar.Click += new System.EventHandler(this.btnTrocar_Click);
+            // 
+            // tbxEn
+            // 
+            this.tbxEn.Location = new System.Drawing.Point(131, 68);
+            this.tbxEn.Name = "tbxEn";
+            this.tbxEn.Size = new System.Drawing.Size(351, 23);
+            this.tbxEn.TabIndex = 7;
+            // 
+            // tbxDe
+            // 
+            this.tbxDe.Location = new System.Drawing.Point(131, 134);
+            this.tbxDe.Name = "tbxDe";
+            this.tbxDe.Size = new System.Drawing.Size(310, 23);
+            this.tbxDe.TabIndex = 8;
+            // 
+            // tbxPa
+            // 
+            this.tbxPa.Location = new System.Drawing.Point(131, 208);
+            this.tbxPa.Name = "tbxPa";
+            this.tbxPa.Size = new System.Drawing.Size(310, 23);
+            this.tbxPa.TabIndex = 9;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(642, 349);
+            this.Controls.Add(this.tbxPa);
+            this.Controls.Add(this.tbxDe);
+            this.Controls.Add(this.tbxEn);
+            this.Controls.Add(this.btnTrocar);
+            this.Controls.Add(this.btnEn);
+            this.Controls.Add(this.lblPa);
+            this.Controls.Add(this.lblDe);
+            this.Controls.Add(this.lblEn);
+            this.Name = "Form";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEn;
+        private System.Windows.Forms.Label lblDe;
+        private System.Windows.Forms.Label lblPa;
+        private System.Windows.Forms.Button btnEn;
+        private System.Windows.Forms.Button btnTrocar;
+        private System.Windows.Forms.TextBox tbxEn;
+        private System.Windows.Forms.TextBox tbxDe;
+        private System.Windows.Forms.TextBox tbxPa;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ nomeArquivo/Form.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nomeArquivo
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEn_Click(object sender, EventArgs e)
+        {
+            tbxEn.Text = string.Empty;
+
+            try
+            {
+                if(folderBrowserDialog1.ShowDialog() != DialogResult.Cancel)
+                {
+                    tbxEn.Text = folderBrowserDialog1.SelectedPath;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Alerta de Erro",MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnTrocar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(tbxEn.Text.Trim() == string.Empty)
+                {
+                    throw new Exception("Selecione a pasta para efetivar a troca de arquivo");
+                }
+
+                if (tbxDe.Text.Trim() == string.Empty) 
+                {
+                    throw new Exception("Informe o valor para a troca.");
+                }
+
+                System.IO.FileInfo[] arquivos = new System.IO.DirectoryInfo(tbxEn.Text).GetFiles();
+
+                foreach (var arquivo in arquivos)
+                {
+                    string novoNome = tbxEn.Text + "\\" + arquivo.Name.Replace(tbxDe.Text, tbxPa.Text);
+
+                    if (!System.IO.File.Exists(novoNome))
+                    {
+                        arquivo.CopyTo(novoNome);
+                        arquivo.Delete();
+                    }
+                }
+                MessageBox.Show("Troca de nome de arquivo efetuada com sucesso...", "aviso",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
